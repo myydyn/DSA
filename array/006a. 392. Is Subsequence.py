6ffe0c991f238d884ddae7e9af1ea1d0b694e25a
Duyen <daphne.duyenhuynh@gmail.com>
2024-09-23 13:25:44 +0700

    subsequence string

---
+++ array/006a. 392. Is Subsequence.py
@@ -0,0 +1,29 @@
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+# A subsequence of a string is a new string that is formed from the original string by deleting 
+# some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+# (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+# Example 1:
+    # Input: s = "abc", t = "ahbgdc"
+    # Output: true
+
+
+from typing import List
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return True if i == len(s) else False
+
+
+
+if __name__ == "__main__":
+    s = "abc"
+    t = "ahbgdc"
+    
+    print(Solution().isSubsequence(s, t))
